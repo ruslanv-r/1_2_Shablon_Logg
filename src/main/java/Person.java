52d53e91cd1e63ad4929b8a0a579b183bd2fb4f4
Romanyuk Ruslan <rvregi@gmail.com>
2022-04-11 23:43:14 +0900

    1

--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -2,6 +2,31 @@ public class Person {
     private final String name;
     private final String surname;
     private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
     private String city;
 
 }
